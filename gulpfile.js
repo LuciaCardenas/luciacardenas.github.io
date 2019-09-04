67345b48477eb310dbb3649a43f7413dd1ed94ad
cristianpb <felipebrokate@gmail.com>
2019-09-04 22:55:37 +0200

    Simplify gulp scripts

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,6 @@ let cleanCSS = require('gulp-clean-css');
 var responsive = require('gulp-responsive');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
- 
-gulp.task('replace', function(){
-  gulp.src(['./_posts/**.md'])
-    // See http://mdn.io/string.replace#Specifying_a_string_as_a_parameter
-    //.pipe(replace(/\/images\//gm, '/assets/img/'))
-    .pipe(replace(/figure:/gm, 'image:'))
-    .pipe(gulp.dest('_posts/'));
-});
 
 gulp.task('responsive', function () {
   return gulp.src('./assets/img/**/{main.png,main.jpg}')
@@ -49,7 +41,7 @@ gulp.task('responsive', function () {
 });
 
 gulp.task('image', function () {
-  gulp.src('./assets/images/*')
+  gulp.src('./assets/img/*')
     .pipe(image({
       pngquant: true,
       optipng: false,
@@ -62,34 +54,25 @@ gulp.task('image', function () {
       concurrent: 10,
       quiet: true // defaults to false
     }))
-    .pipe(gulp.dest('./assets/images/'));
+    .pipe(gulp.dest('./assets/img'));
 });
 
 gulp.task('sass', function () {
   return gulp.src('./_sass/main.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css/dist'));
-});
-
-// purify removes unused CSS classes
-gulp.task('purify', ['sass'],  function() {
-  return gulp.src('./assets/css/dist/main.css')
     .pipe(purify(['./_includes/**.html', './_layouts/**.html', './_pages/**.html', './blog/**.html'], {info: true}))
     .pipe(replace(/!important/gm, ''))
-    .pipe(gulp.dest('./assets/css/dist'));
-});
- 
-gulp.task('concat', ['purify'], function() {
-  return gulp.src(['./assets/css/dist/main.css', './assets/css/syntax.css'])
-    .pipe(concat('main.css'))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(rename({ suffix: '-min' }))
     .pipe(gulp.dest('./_includes/'));
 });
 
-gulp.task("watch", ["purify", "sass", "concat"], function() {
-  gulp.watch(["./_sass/main.scss", "./_sass/_variables.scss"], ["sass", "purify", "concat"])
-  gulp.watch(["./_pages/**", "./_layouts/**", "./_includes/**.html", "./blog/*"], ["purify"]);
+gulp.task("watch", ["sass"], function() {
+  gulp.watch(["./_sass/main.scss",
+    "./_sass/_variables.scss",
+    "./_pages/**",
+    "./_layouts/**",
+    "./_includes/**.html"], ["sass"])
 })
 
 gulp.task('default', ['watch'], function(){});
